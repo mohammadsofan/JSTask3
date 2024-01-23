fcfe64474a4570a710cf4e49379b555e3ce72aeb
mohammadsofan <mohammadsof72@gmail.com>
2024-01-23 16:38:33 +0200

    v0

---
+++ js/main.js
@@ -0,0 +1,21 @@
+let parent = document.querySelector(".parent");
+
+async function getProducts(){
+   let response = await fetch("https://dummyjson.com/products");
+   let data = await response.json();
+   let products = data.products;
+   
+   const result = products.map(function (product){
+        return `
+        <div class="child">
+        <img src='${product.thumbnail}'/>
+        <h2>${product.title}</h2>
+        </div>
+        `;
+   }).join('');
+
+   parent.innerHTML = result;
+}
+
+
+getProducts();
